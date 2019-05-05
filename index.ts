09125dadc26611967975e3e57a90afa4e4b06529
손량 <loop.infinitely@gmail.com>
2019-05-05 23:03:16 +0900

    finished!!!

--- index.ts
+++ index.ts
@@ -1,13 +1,35 @@
-import {Engine, Render, World, Bodies} from 'matter-js';
+import {
+    Bodies,
+    Constraint,
+    Engine,
+    Mouse,
+    MouseConstraint,
+    Render,
+    World,
+} from 'matter-js';
 
 const engine = Engine.create();
 const renderer = Render.create({
     element: document.body,
-    engine: engine
+    engine: engine,
+    options: {
+        wireframes: false
+    }
 });
 
+const mouse = Mouse.create(renderer.canvas);
+const mouseConstraint = MouseConstraint.create(engine, {
+    constraint: {
+        render: {visible: false}
+    },
+    mouse: mouse,
+});
 const ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true});
-World.add(engine.world, [ground]);
+const pendulum = Bodies.circle(400, 400, 50);
+const string = Constraint.create({pointA: {x: 400, y: 0}, bodyB: pendulum});
+World.add(engine.world, [ground, pendulum]);
+World.add(engine.world, mouseConstraint);
+World.add(engine.world, string);
 
 Engine.run(engine);
 Render.run(renderer);
