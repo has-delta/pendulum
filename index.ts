307dac7665fb14564fec75f7d7b5bb1a555a9741
손량 <loop.infinitely@gmail.com>
2019-05-05 22:29:27 +0900

    typescript

---
+++ index.ts
@@ -0,0 +1,15 @@
+import {Engine, Render, World, Bodies} from 'matter-js';
+
+const engine = Engine.create();
+const renderer = Render.create({
+    element: document.body,
+    engine: engine
+});
+
+const ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true});
+World.add(engine.world, [ground]);
+
+Engine.run(engine);
+Render.run(renderer);
+
+Engine.run(engine);
